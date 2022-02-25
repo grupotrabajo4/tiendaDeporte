da52fd77b0754e59431ba00e8adbda476cf00a73
Dina <dinaalmeria@gmail.com>
2022-02-25 10:55:57 +0100

    modificacion en la clase cliente

--- TiendaDeporteGrupo/src/tiendadeportegrupo/Cliente.java
+++ TiendaDeporteGrupo/src/tiendadeportegrupo/Cliente.java
@@ -13,43 +13,72 @@ public class Cliente
 {
     public class Cliente implements Empleado 
     {
-    //atributos
-    private String nombre;
-    private String apellido;
-    private String nacimiento;
-    private String dni;
-    private enum tipoEmpleado { POTENCIAL, NUEVO, VIP, MOROSO };
-    
-    //constructor
-    public Cliente(enum tipoEmpleado)
-    {
-        this.tipoEmpleado=tipoEmpleado;
-    }
-    
-    /**
-     * <H2>Método para dar de alta a nuevos clientes</H2>
-     * @author dinam
-     * @param dni del cliente tipo String ya que es alfanumerico
-     * @return String creado donde sale los datos de alta de un cliente
-    */
-    
+        //atributos
+        private String nombre;
+        private String apellido;
+        private String nacimiento;
+        private String dni;
+        private enum tipoEmpleado { POTENCIAL, NUEVO, VIP, MOROSO };
 
-    public static String Alta()
-    {
-        System.out.println("Introduce el nombre: ")
-        String nombre = pedirDatos.nextLine();
-        System.out.println("Introduce el apellido: ")
-        String apellido = pedirDatos.nextLine();
-        System.out.println("Introduce lugar de nacimiento: ")
-        String nacimiento = pedirDatos.nextLine();
-        System.out.println("Introduce el dni: ")
-        String dni = pedirDatos.nextLine();
+        //constructor
+        public Cliente(enum tipoEmpleado)
+        {
+            this.tipoEmpleado=tipoEmpleado;
+        }
+
+        /**
+         * <H2>Método para dar de alta a nuevos clientes</H2>
+         * @author dinam
+         * @param dni del cliente tipo String ya que es alfanumerico
+         * @return String creado donde sale los datos de alta de un cliente
+        */
+
         
-        return "nombre='" + nombre + '\'' + ", apellido='" + apellido + '\'' +
-                ", lugar de nacimiento=" + nacimiento +  '\'' +
-                ", dni=" + dni:
-    }
-}
+        public static String Alta()
+        {
+            System.out.println("Introduce el nombre: ")
+            String nombre = pedirDatos.nextLine();
+            System.out.println("Introduce el apellido: ")
+            String apellido = pedirDatos.nextLine();
+            System.out.println("Introduce lugar de nacimiento: ")
+            String nacimiento = pedirDatos.nextLine();
+            System.out.println("Introduce el dni: ")
+            String dni = pedirDatos.nextLine();
 
+            return "nombre='" + nombre + '\'' + ", apellido='" + apellido + '\'' +
+                    ", lugar de nacimiento=" + nacimiento +  '\'' +
+                    ", dni=" + dni:
+        }
+    }
     
+    public int getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public int getApellido() {
+    return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    
+    public int getNacimiento() {
+    return nacimiento;
+    }
+
+    public void setNacimiento(String nacimiento) {
+        this.nacimiento = nacimiento;
+    }
+    
+    public int getDni() {
+    return dni;
+    }
+
+    public void setDni(String dni) {
+    this.dni = dni;
 }
