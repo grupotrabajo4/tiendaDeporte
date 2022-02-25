3be3c9193cc9858384b7510857c20c35e89816e6
Miguel <maalivent@gmail.com>
2022-02-25 12:03:40 +0100

    Clase Principal comentada con Javadoc

--- tiendaDeporte/src/tiendaDeporte/Principal.java
+++ tiendaDeporte/src/tiendaDeporte/Principal.java
@@ -1,21 +1,54 @@
-
 package tiendadeporte;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
+
+ * <H2>Clase Principal</H2>
+
+ * Crea una lista de clientes, proveedores y artículos y los muestra por pantalla.
+
  *
- * @author Miguel
+
+ * @author Lagartijo676
+
+ * @version 0.1
+
  */
 public class Principal {
     
+	/**
+
+	 * <H2>Atributo escaner</H2>
+
+	 * Lee por pantalla
+
+	 * @param System.in
+
+	 */
     static Scanner escaner = new Scanner(System.in);
     
+    /**
+
+     * <H2>Atributos Cliente, Articulo y Proveedor</H2>
+
+     * Listas de clientes, artículos y proveedores
+
+     */
     static ArrayList<Cliente> clientes = new ArrayList<Cliente>();
     static ArrayList<Articulo> articulos = new ArrayList<Articulo>();
     static ArrayList<Proveedor> proveedores = new ArrayList<Proveedor>();
     
+    /**
+
+     * <H2>Método introducirClientes</H2>
+
+     * Introduce clientes en la lista de clientes
+
+     * @param n número de clientes a introducir
+
+     */
     public static void introducirClientes(int n){
         for(int i=0;i<n;i++){
             System.out.println("Vamos a introducir " + n + " clientes:");
@@ -23,7 +56,17 @@ public class Principal {
             clientes.add(new Cliente(escaner.nextLine()));
         }
     }
-    
+    /**
+
+ 	/**
+
+     * <H2>Método introducirProveedores</H2>
+
+     * Introduce proveedores en la lista de proveedores
+
+     * @param n número de proveedores a introducir
+
+     */
     public static void introducirProveedores(int n){
         for(int i=0;i<n;i++){
             System.out.println("Vamos a introducir " + n + " proveedores:");
@@ -32,6 +75,15 @@ public class Principal {
         }
     }
     
+    /**
+
+     * <H2>Método introducirArticulos</H2>
+
+     * Introduce artículos en la lista de artículos
+
+     * @param n número de artículos a introducir
+
+     */
     public static void introducirArticulos(int n){
         for(int i=0;i<n;i++){
             System.out.println("Vamos a introducir " + n + " articulos:");
@@ -40,6 +92,13 @@ public class Principal {
         }
     }
     
+    /**
+
+     * <H2>Método main</H2>
+
+     * Introduce los clientes, proveedores y artículos en sus listas correspondientes y los muestra por pantalla.
+
+     */
     public static void main(String[] args){
         
         introducirClientes(4);
