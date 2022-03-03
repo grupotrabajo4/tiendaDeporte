0bb10098d0969a5ba55dcf034fccb1aedcc0742e
JOSMA <josemanuelgarciatrave@gmail.com>
2022-03-03 13:14:48 +0100

    validacion cambios

---
+++ tiendaDeporte/doc/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"tiendaDeporte"}];updateSearchResults();
--- tiendaDeporte/src/tiendaDeporte/Proveedor.java
+++ tiendaDeporte/src/tiendaDeporte/Proveedor.java
@@ -1,6 +1,6 @@
 package tiendaDeporte;
 /**
- * <H2>CREACION CLASE PROVEEDOR<H2>
+ * <H2>CREACION CLASE PROVEEDOR</H2>
  * @author BYJOSEGRC
  * @version 0.1
  */
@@ -8,31 +8,31 @@ package tiendaDeporte;
 public class Proveedor extends Persona {
 
 	/**
-	 * <H2>ATRIBUTOS<H2>
+	 * <H2>ATRIBUTOS</H2>
 	 * @author BYJOSEGRC
 	 * @version 0.1
 	 */
 	
-	String nombreProveedor;
-	String apellido1;
-	String apellido2;
+	private String nombreProveedor;
+	private String apellido1;
+	private String apellido2;
 	
 	
 	
 	/**
-	 * <H2>CONSTRUCTOR<H2>
+	 * <H2>CONSTRUCTOR</H2>
 	 * @author BYJOSEGRC
 	 * @version 0.1
 	 */
 	
 	public Proveedor(String nombreProveedor,String apellido1, String apellido2){
-		this.nombreProveedor=nombreProveedor;
-		this.apellido1=apellido1;
-		this.apellido2=apellido2;
+		this.setNombreProveedor(nombreProveedor);
+		this.setApellido1(apellido1);
+		this.setApellido2(apellido2);
 	}
 	
 	/**
-	 * <H2Funcion_dar_alta_a_Proveedor<H2>
+	 * <H2Funcion_dar_alta_a_Proveedor</H2>
 	 * @author BYJOSEGRC
 	 * @return nuevoProveedor
 	 * @version 0.1
@@ -45,7 +45,7 @@ public class Proveedor extends Persona {
 	}
 	
 	/**
-	 * <H2GET nombreProveedor<H2>
+	 * <H2GET nombreProveedor</H2>
 	 * @author BYJOSEGRC
 	 * @return nombreProveedor
 	 * @version 0.1
@@ -55,7 +55,7 @@ public class Proveedor extends Persona {
     }
 	
 	/**
-	 * <H2GET nombreProveedor<H2>
+	 * <H2GET nombreProveedor</H2>
 	 * @author BYJOSEGRC
 	 * @return apellido1
 	 * @version 0.1
@@ -66,7 +66,7 @@ public class Proveedor extends Persona {
     }
 	
 	/**
-	 * <H2GET nombreProveedor<H2>
+	 * <H2GET nombreProveedor</H2>
 	 * @author BYJOSEGRC
 	 * @return apellido2
 	 * @version 0.1
@@ -74,6 +74,18 @@ public class Proveedor extends Persona {
 	public String getApellido2() {
         return apellido2;
     }
+
+	void setNombreProveedor(String nombreProveedor) {
+		this.nombreProveedor = nombreProveedor;
+	}
+
+	void setApellido1(String apellido1) {
+		this.apellido1 = apellido1;
+	}
+
+	void setApellido2(String apellido2) {
+		this.apellido2 = apellido2;
+	}
 	
 
 }
